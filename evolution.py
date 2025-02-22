73c8d0f3cdc44555c8bc52378ddcb67e08c6d501
prione <92021420+prione@users.noreply.github.com>
2025-02-22 12:59:49 +0900

    Update evolution.py

--- evolution.py
+++ evolution.py
@@ -3,23 +3,21 @@ import re
 import numpy as np
 import config
 
-def evolve(genotypes, scores):
+def evolve(genotypes):
     new_genotypes = []
     for _ in range(config.genotypes_num//2):
-        p1, p2 = select_parents(scores)
-        children = create_children(genotypes[p1], genotypes[p2])
+        paprent1, paprent2 = select_parents(genotypes)
+        children = create_children(paprent1, paprent2)
         new_genotypes.extend(children)
-    
     return new_genotypes
 
-def select_parents(scores):
+def select_parents(genotypes):
     alpha = 0.3
-    fertile_genotypes = np.arange(0, len(scores))
+    fertile_genotypes = np.arange(0, len(genotypes))
     weights = np.exp(-alpha * fertile_genotypes)
     weights /= weights.sum() 
     p1, p2 = np.random.choice(fertile_genotypes, size=2, replace=True, p=weights)
-    
-    return p1, p2
+    return genotypes[p1], genotypes[p2]
 
 def create_children(prompt1, prompt2):
     text = llm.generate(evo_prompt(prompt1, prompt2), 1)
@@ -36,8 +34,9 @@ def create_children(prompt1, prompt2):
 def evo_prompt(prompt1, prompt2): 
     return f"""
 Please follow the instructions based on the Genetic Algorithm step-by-step to generate better prompts in Japanese.
+However, be careful not to change the purpose of the prompt.
 Step 1. Exchange some words or sentences in the following prompts and generate two new prompts:
-# prompt1: {prompt1}
-# prompt2: {prompt2}
+#prompt1: {prompt1}
+#prompt2: {prompt2}
 Step 2. Modify each prompt generated in Step 1 (add or remove some sentences, or change the expression of some sentences) and generate two final prompts, each of which is bracketed with <prompt> and </prompt>.
 """
