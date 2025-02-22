e82c1416050d40ecc87b04c6770e50b55ed5adcf
prione <92021420+prione@users.noreply.github.com>
2025-02-22 13:02:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from evolution import evolve
 from individual import individual
 import systemprompt
 import config
-import pickle
-
+import dill
 
 def run(genotypes:list, expected:str, generation:int):
     result = [] 
@@ -14,8 +13,8 @@ def run(genotypes:list, expected:str, generation:int):
     for gen in range(generation):
         phenotypes = []
         individuals = []
-        for p in genotypes:
-            phenotypes.append(llm.generate(p,0))
+        for g in genotypes:
+            phenotypes.append(llm.generate(g,0))
         
         genotypes, phenotypes, scores = evaluate(genotypes, phenotypes, expected)
         print(f"{gen}th best score: {scores[0]}")
@@ -24,6 +23,7 @@ def run(genotypes:list, expected:str, generation:int):
         for prompt, answer, score in zip(genotypes, phenotypes, scores):
             individuals.append(individual(prompt, answer, score))
         result.append(individuals)
+        save(result)
         
         genotypes = evolve(genotypes, scores)
     
@@ -31,9 +31,8 @@ def run(genotypes:list, expected:str, generation:int):
     
 def save(result):
     with open(f"{config.save_dir}/result.pkl", "wb") as f:
-        pickle.dump(result, f)
+        dill.dump(result, f)
  
 if __name__ == "__main__":
     result = run(config.initial_genotypes, config.expected_answer, config.generation)
-    save(result)
     
